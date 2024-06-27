4b6fd322f7b258974d9aec2266f71eb65d09ff3a
vipcaqb <hoangloc921999@gmail.com>
2024-06-27 12:11:31 +0700

    hh

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3000;
 const weatherApiKey = process.env.WEATHER_KEY;
 const pixaApiKey = process.env.PIXA_KEY;
-const geonameApiKey = process.env.GEONAME_KEY;
+const geonamesUsername = process.env.GEONAME_KEY;
 
 app.use(express.static('dist'));
 
@@ -15,27 +18,47 @@ app.get('/', (req,res)=>{
 
 app.get('/api/trip', getTrip);
 
-app.get('/api/weather', getWeatherData);
-
-app.get('/api/coordinates', getCoordinates);
-
-app.get('/api/photo', getPhoto);
-
-const getTrip = async (req,res,next) =>{
+async function getTrip(req,res,next){
 
   const { cityName, days } = req.query;
   
   if(cityName == undefined || cityName ==="" ){
     return res.status(400).json({error: 'City name is required'})
   }
+  // Get photo
+const {photoUrl, photoErrMsg} = await getPhotoUrl(cityName);
 
-  const {latitude, longitude, errMsg} = await getCoordinates(cityName)
+if(photoErrMsg != undefined){
+  return res.status(400).json({errMsg:photoErrMsg})
+}
+// check days 
+if(days >7){
+  return res.status(200).json({
+    photoUrl: photoUrl
+  });
+}
+else {
+  // Get coordinates
+  const {latitude, longitude, coorErrMsg} = await getCoordinates(cityName);
 
-  if(errMsg != undefined){
-    return res.status(400).json({errMsg:errMsg})
+  if(coorErrMsg != undefined){
+    return res.status(400).json({errMsg:coorErrMsg});
   }
 
-
+  // Get weather info
+  const {max_temp, min_temp, description, weatherErrMsg} = await getWeatherData(latitude, longitude, days);
+  
+  if(weatherErrMsg != undefined){
+    return res.status(400).json({errMsg:weatherErrMsg});
+  }
+  else {
+    return res.status(200).json({
+      max_temp:max_temp,
+      min_temp:min_temp,
+      description:description
+    })
+  }
+}
 }
 
 /*
@@ -43,7 +66,7 @@ return {
               cityName: cityName,
               latitude: city.lat,
               longitude: city.lng,
-              errMsg: ''
+              coorErrMsg: ''
             }
 */
 const getCoordinates = async (cityName) =>{
@@ -59,13 +82,13 @@ const getCoordinates = async (cityName) =>{
             }
         } else {
             return {
-              errMsg: 'City not found'
+              coorErrMsg: 'City not found'
             }
         }
     } catch (error) {
         console.error(error);
         return {
-          errMsg: 'An error occurred while fetching data from Geonames'
+          coorErrMsg: 'An error occurred while fetching data from Geonames'
         }
     }
 }
@@ -74,35 +97,37 @@ const getWeatherData = async (lat, lng, days) =>{
     try {
     const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&days=${days}&key=${weatherApiKey}`);
     const data = await response.json();
-    const targetDateData = data[days];
+    const targetDateData = data.data[days];
+    console.log(targetDateData)
     return {
       max_temp: targetDateData.max_temp,
       min_temp: targetDateData.min_temp,
       description: targetDateData.weather.description
     }
   } catch (error) {
+    console.log(error);
     return {
-      errMsg: 'Failed to fetch weather data'
+      weatherErrMsg: 'Failed to fetch weather data'
     }
   }
 }
 
-const getPhotoUrl = async(location, pixaApiKey) =>{
+const getPhotoUrl = async(location) =>{
   try {
     const response = await fetch(`https://pixabay.com/api/?key=${pixaApiKey}&q=${location}&image_type=photo`);
     const data = await response.json();
     if(data.total = 0){
       return {
-        errMsg: 'Cannot fount photo of your city'
+        photoErrMsg: 'Cannot fount photo of your city'
       }
     }
     return {
-      photoUrl: hits[0].largeImageURL
+      photoUrl: data.hits[0].largeImageURL
     }
   } catch (error) {
     console.log(error);
     return {
-      errMsg: 'Failed to fetch photo data'
+      photoErrMsg: 'Failed to fetch photo data'
     }
   }
 }
